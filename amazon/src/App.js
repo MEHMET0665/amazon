12e64b23efd6062da99fff38d22b151f4bbec40f
Memet <meavci@gmail.com>
2021-05-11 13:59:42 -0500

    create header

---
+++ amazon/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    //BEM
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ amazon/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCS7ABJ5tjCl5yZ9UDT9-zBtYYBN3wPCC8",
+  authDomain: "fir-4a2ae.firebaseapp.com",
+  projectId: "fir-4a2ae",
+  storageBucket: "fir-4a2ae.appspot.com",
+  messagingSenderId: "296100496248",
+  appId: "1:296100496248:web:9f3481310a56478693fe8d",
+  measurementId: "G-Q4LLVQQH1W"
+};
